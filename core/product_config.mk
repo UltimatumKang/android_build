840ea983975f03e26a733fd16741b03af8fa0800
Daniel MM <daxx4182@gmail.com>
2013-05-14 20:20:20 +0800

    More U-Kang. :P

--- core/product_config.mk
+++ core/product_config.mk
@@ -185,16 +185,16 @@ ifneq ($(strip $(TARGET_BUILD_APPS)),)
 all_product_configs := $(call get-product-makefiles,\
     $(SRC_TARGET_DIR)/product/AndroidProducts.mk)
 else
-  ifneq ($(CARBON_BUILD),)
-    all_product_configs := $(shell ls device/*/$(CARBON_BUILD)/carbon.mk)
+  ifneq ($(UKG_BUILD),)
+    all_product_configs := $(shell ls device/*/$(UKG_BUILD)/ukg.mk)
   else
     # Read in all of the product definitions specified by the AndroidProducts.mk
     # files in the tree.
     all_product_configs := $(get-all-product-makefiles)
-  endif # CARBON_BUILD
+  endif # UKG_BUILD
 endif
 
-ifeq ($(CARBON_BUILD),)
+ifeq ($(UKG_BUILD),)
 # Find the product config makefile for the current product.
 # all_product_configs consists items like:
 # <product_name>:<path_to_the_product_makefile>
